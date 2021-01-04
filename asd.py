2fda5ac8b58901e645f7531a067ff3e871b55331
khsh940301 <khsh940301@gmail.com>
2021-01-05 00:05:08 +0900

    2021-01-05 BOJ Update

--- asd.py
+++ asd.py
@@ -1,65 +1,116 @@
-import sys, math
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
 
-def init(arr, tree, node, start, end):
-    if start == end:
-        tree[node] = arr[start]
-        return tree[node]
 
-    mid = (start + end) // 2
-    tree[node] = init(arr, tree, node * 2, start, mid) ^ \
-                 init(arr, tree, node * 2 + 1, mid + 1, end)
-    return tree[node]
+m_xTranslation, m_yTranslation, m_zTranslation = 0.0, 0.0, -10.0
+m_xRotation, m_yRotation, m_zRotation = 0.0, 0.0, 0.0
+m_xScaling, m_yScaling, m_zScaling = 1.0, 1.0, 1.0
+x_last, y_last = 0, 0
 
-def updateLazyTree(tree, lazy, node, start, end):
-    if lazy[node] != 0:
-        tree[node] ^= lazy[node] * ((end - start + 1) % 2)
-        if start != end:
-            lazy[node * 2] ^= lazy[node]
-            lazy[node * 2 + 1] ^= lazy[node]
-        lazy[node] = 0
 
-def updateValue(tree, lazyTree, node, start, end, left, right, value):
-    updateLazyTree(tree, lazyTree, node, start, end)
+def Draw():
+    size = [2.0, 2.0, 2.0]
 
-    if (right < start or end < left): return tree[node]
-    elif (left <= start and end <= right):
-        tree[node] ^= value * ((end - start + 1) % 2)
-        if start != end:
-            lazyTree[node * 2] ^= value
-            lazyTree[node * 2 + 1] ^= value
-        return tree[node]
+    point1 = [size[0] / 2.0, size[1] / 2.0, size[2] / -2.0]
+    point2 = [size[0] / 2.0, size[1] / 2.0, size[2] / 2.0]
+    point3 = [size[0] / 2.0, size[1] / -2.0, size[2] / 2.0]
+    point4 = [size[0] / 2.0, size[1] / -2.0, size[2] / -2.0]
+    point5 = [size[0] / -2.0, size[1] / -2.0, size[2] / 2.0]
+    point6 = [size[0] / -2.0, size[1] / 2.0, size[2] / 2.0]
+    point7 = [size[0] / -2.0, size[1] / 2.0, size[2] / -2.0]
+    point8 = [size[0] / -2.0, size[1] / -2.0, size[2] / -2.0]
 
-    mid = (start + end) // 2
-    tree[node] = updateValue(tree, lazyTree, node * 2, start, mid, left, right, value) ^ \
-                 updateValue(tree, lazyTree, node * 2 + 1, mid + 1, end, left, right, value)
-    return tree[node]
+    glBegin(GL_QUADS)
 
-def xorValue(tree, lazy, node, start, end, left, right):
-    updateLazyTree(tree, lazy, node, start, end)
-    if (right < start or end < left): return 0
-    elif (left <= start and end <= right): return tree[node]
+    glVertex3fv(point1)  # TOP
+    glVertex3fv(point2)
+    glVertex3fv(point6)
+    glVertex3fv(point7)
 
-    mid = (start + end) // 2
-    return xorValue(tree, lazy, node * 2, start, mid, left, right) ^ \
-           xorValue(tree, lazy, node * 2 + 1, mid + 1, end, left, right)
+    glVertex3fv(point3)  # Bottom
+    glVertex3fv(point4)
+    glVertex3fv(point8)
+    glVertex3fv(point5)
 
-def solution():
-    N = int(sys.stdin.readline())
-    arr = list(map(int, sys.stdin.readline().split()))
+    glVertex3fv(point2)  # Front
+    glVertex3fv(point3)
+    glVertex3fv(point5)
+    glVertex3fv(point6)
 
-    height = int(math.ceil(math.log2(N)))
-    treeSize = 1 << (height + 1)
-    segmentTree = [0] * treeSize
-    lazySegmentTree = [0] * treeSize
+    glVertex3fv(point7)  # Back
+    glVertex3fv(point8)
+    glVertex3fv(point4)
+    glVertex3fv(point1)
 
-    init(arr, segmentTree, 1, 0, N-1)
-    M = int(sys.stdin.readline())
-    for i in range(M):
-        command = list(map(int, sys.stdin.readline().split()))
+    glVertex3fv(point6)  # Left
+    glVertex3fv(point5)
+    glVertex3fv(point8)
+    glVertex3fv(point7)
 
-        if command[0] == 1:
-            updateValue(segmentTree, lazySegmentTree, 1, 0, N-1, command[1], command[2], command[3])
-        else:
-            print(xorValue(segmentTree, lazySegmentTree, 1, 0, N-1, command[1], command[1]))
+    glVertex3fv(point1)  # Right
+    glVertex3fv(point4)
+    glVertex3fv(point3)
+    glVertex3fv(point2)
 
-solution()
+    glEnd()
+
+def initFun():
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glShadeModel(GL_FLAT)
+
+def displayFun():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(1.0, 1.0, 1.0)
+    glLoadIdentity()
+    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+
+    glPushMatrix()
+    glTranslatef(m_xTranslation, m_yTranslation, m_zTranslation)
+    glRotatef(m_xRotation, 1.0, 0.0, 0.0)
+    glRotatef(m_yRotation, 0.0, 1.0, 0.0)
+    glRotatef(m_zRotation, 0.0, 0.0, 1.0)
+    glScalef(m_xScaling, m_yScaling, m_zScaling)
+    Draw()
+    glPopMatrix()
+    glFlush()
+    glutPostRedisplay()
+
+def reshapeFunc(w, h):
+    glViewport(0, 0, w, h)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0)
+    glMatrixMode(GL_MODELVIEW)
+
+def MyMouseClick(Button, State, x, y):
+    global m_zTranslation
+    print(Button, State, x, y)
+    if Button == 3:
+        m_zTranslation += 1.0
+    elif Button == 4:
+        m_zTranslation += -1.0
+    else:
+        x_last, y_last = x, y
+
+def MyMouseMove(x, y):
+    global m_xRotation, m_yRotation, x_last, y_last
+    m_speedRotation = (1.0 / 100.0)
+    m_xRotation -= (float)(y - y_last) * m_speedRotation
+    m_yRotation += (float)(x - x_last) * m_speedRotation
+
+
+if __name__=='__main__':
+    glutInit()
+    glutInitWindowSize(500, 500)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow(b"TEST")
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutDisplayFunc(displayFun)
+    glutReshapeFunc(reshapeFunc)
+
+    glutMouseFunc(MyMouseClick)
+    glutMotionFunc(MyMouseMove)
+
+    initFun()
+    glutMainLoop()
